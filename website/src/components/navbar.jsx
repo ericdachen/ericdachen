50699388f86a9592185d2c9b58904862e9c72efa
Eric Chen <e56chen@uwaterloo.ca>
2021-04-01 21:51:34 -0400

    Styling components

--- website/src/components/navbar.jsx
+++ website/src/components/navbar.jsx
@@ -1,23 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import './navbar.scss'
 import { AppBar, Tabs, Tab } from '@material-ui/core';
+import { Typography } from '@material-ui/core/Typography';
 import { TabPanel, TabContext } from '@material-ui/lab';
 
+const StyledTabs = withStyles({
+    indicator: {
+      display: "flex",
+      justifyContent: "center",
+      backgroundColor: "transparent",
+      "& > span": {
+        maxWidth: 40,
+        width: "100%",
+        backgroundColor: "#635ee7"
+      }
+    }
+  })((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+  
+  const StyledTab = withStyles((theme) => ({
+    root: {
+      textTransform: "none",
+      color: "#fff",
+      fontWeight: theme.typography.fontWeightRegular,
+      fontSize: theme.typography.pxToRem(15),
+      marginRight: theme.spacing(1),
+      "&:focus": {
+        opacity: 1
+      }
+    }
+  }))((props) => <Tab disableRipple {...props} />);
+
+  const StyledAppBar = withStyles ((theme) => ({
+      root: {
+          backgroundColor: "#E8FFFC",
+      }
+  }))((props) => <AppBar {...props} />);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1
+    },
+    padding: {
+      padding: theme.spacing(3)
+    },
+    demo1: {
+      backgroundColor: theme.palette.background.paper
+    },
+    demo2: {
+      backgroundColor: "#2e1534"
+    }
+  }));
 
 class NavBar extends React.Component {
     render() {
         return (
-            <>
-                <AppBar className="appbar" position="static">
-                    <Tabs aria-label="simple tabs example" className="tab" value="0">
-                        <Tab className="tabs" label="Home" value="0"/>
-                        <Tab className="tabs" label="Showcase" value="1"/>
-                        <Tab className="tabs" label="Contact" value="2"/>
-                        <Tab className="tabs" label="Resume" value="3"/>
-                    </Tabs>
-                </AppBar>
-            </>
+            <div>
+                <StyledAppBar className="appbar" position="static">
+                    <StyledTabs aria-label="simple tabs example" className="tab" value="0">
+                        <StyledTab label="Home" value="0"/>
+                        <StyledTab label="Showcase" value="1"/>
+                        <StyledTab label="Contact" value="2"/>
+                        <StyledTab label="Resume" value="3"/>
+                    </StyledTabs>
+                </StyledAppBar>
+            </div>
         );
     }
 }
