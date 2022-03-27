4c2e9a19f163a016b2fa474eb07b90f9df32ba03
ericdachen <erche3742@gmail.com>
2022-03-27 17:32:16 -0400

    Adding navbar and modal

---
+++ ericdachen/components/navbar/NavBar.tsx
@@ -0,0 +1,83 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import ProfileModal from "../social-modal/ProfileModal";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Modal size="sm" isOpen={isOpen} onClose={onClose} motionPreset="scale">
+        <ModalOverlay />
+        <ModalContent>
+          <ProfileModal />
+        </ModalContent>
+      </Modal>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Menu>
+            <MenuButton
+              as={Button}
+              rounded={"full"}
+              variant={"link"}
+              cursor={"pointer"}
+              minW={0}
+              onClick={onOpen}
+            >
+              <Avatar size={"sm"} src={"headshot.JPG"} />
+            </MenuButton>{" "}
+          </Menu>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+export default NavBar;
