13caef4b268b429a20dd37408cd5603d26f9483a
Eric Chen <erche3742@gmail.com>
2022-11-08 03:52:38 +0100

    Delete GifProfile.tsx
    removing gifprofile

--- components/home/GifProfile.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Lottie from "lottie-react";
-import HappyCode from "../../public/happycode.json";
-
-const App = () => <Lottie animationData={HappyCode} loop={true} />;
-
-export default App;
